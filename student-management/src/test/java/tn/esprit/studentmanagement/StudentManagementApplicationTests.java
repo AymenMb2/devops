2852461c1c9ffb4152db01ce670d50f849ef9c7f
Aymen_mb <161446378+AymenMb2@users.noreply.github.com>
2026-03-02 10:49:29 +0100

    Update StudentManagementApplicationTests.java

--- student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -1,13 +1,20 @@
 package tn.esprit.studentmanagement;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class StudentManagementApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(applicationContext, "Application context should not be null");
     }
 
 }
